d1c9d81d62e6dfc909cf4916c41fe72802009190
Marcos Garcia de Lima <marckfree@gmail.com>
2020-12-06 00:32:12 -0300

    Add next app

---
+++ next-app/pages/about/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+function About() {
+  return (
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
+
+export default About
